f4883e78bc8665f594b1d5b414c8cc9db8110491
Dzmitry_Betska <d.betska@gmail.com>
2023-08-25 18:45:19 +0300

    creating a config server for hotel-project settings

---
+++ src/main/java/by/project/settings/SettingsApplication.java
@@ -0,0 +1,14 @@
+package by.project.settings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SettingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettingsApplication.class, args);
+    }
+}
