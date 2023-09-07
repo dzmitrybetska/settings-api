2831cfe751cb21017180793030109a6c5202a6f1
Dzmitry_Betska <d.betska@gmail.com>
2023-09-07 18:00:59 +0300

    configuring the application to work with eureka-server

--- src/main/java/by/project/settings/SettingsApplication.java
+++ src/main/java/by/project/settings/SettingsApplication.java
@@ -2,9 +2,11 @@ package by.project.settings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SettingsApplication {
 
